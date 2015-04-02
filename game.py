7e4fde9a703faf12325c1719485d93876202e8f5
Joost Wagenaar <JCWagenaar@gmail.com>
2015-04-02 21:38:58 +0200

    Eerste vlugge beginnetje tussen het koken door.

---
+++ game.py
@@ -0,0 +1,18 @@
+# Heuristieken.
+# Joost Jason en Joren.
+# RushHour.
+
+class Car(object):
+    def __init__(self, orientation, start_coordinate, length):
+        self.orientation = orientation
+        self.start_coordinate = start_coordinate
+        self.length = length
+
+    # Check toevoegen om te checken of orientation "horizontal" of "vertical" is.
+    # Check toevoegen of start_coordinate bestaat.
+    # Check toevoegen of start_coordinate + length past in het bord.
+    # Check toevoegen of alle in te vullen coordinaten niet al bezet zijn.
+    
+
+
+        
